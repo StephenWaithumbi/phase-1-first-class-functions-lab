983ca247d133277430894793df6d4b0444aebfba
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-09-29 18:49:14 +0300

    done with advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (firstTwoDrivers) {
+    return firstTwoDrivers.slice(0, 2); 
+};
+
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const returnLastTwoDrivers = function(lastTwoDrivers){
+    return lastTwoDrivers.slice(2,4);
+}
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(selectingDrivers[0](drivers));
+console.log(selectingDrivers[1](drivers)); 
+
+function createFareMultiplier(multiplier){
+    return (fare) => {
+        return fare * multiplier;
+    };
+}
+const fareQuadPlier = createFareMultiplier(4);
+console.log(fareQuadPlier(100));
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(100));
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(100));
+
+function selectDifferentDrivers(arrayOfDrivers, selectingFunction) {
+    return selectingFunction(arrayOfDrivers);
+};
